7d2829845b6ff054f3d7668b852daf33e1e127b8
Euyon <121988424+eswhon@users.noreply.github.com>
2023-04-17 12:01:09 +0800

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -97,7 +97,7 @@ async def help_handler(client, msg):
     db = Database(msg.from_user.id)
     member = db.get_data_pelanggan()
     pesan = "Silahkan gunakan format berikut untuk mengirim pesan\n"
-    pesan += '#Boy / #Girl untuk Mencari Pasangan, Teman , Partner FWB \n#Ask untuk Bertanya \n#Story untuk Berbagi Cerita \n#Spill untuk Spill Masalah \n#Find untuk Mencari VCS \n'
+    pesan += 'Gunakan Format Tag # sesuai dengan topik'
     if member.status == 'admin':
         pesan += '\nHanya Admin\n'
         pesan += '/tf_coin — transfer coin\n'
